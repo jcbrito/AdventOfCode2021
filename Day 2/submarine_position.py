4583fb32214f420c60484aef86244a820edbb700
jbrito <jcbritov2@gmail.com>
2021-12-02 04:21:59 -0500

    Add files via upload
    inputs for day 2
    
    solved day 2 (1/2)

---
+++ Day 2/submarine_position.py
@@ -0,0 +1,18 @@
+depth = 0
+horizontal = 0
+
+file = open("inputs.txt")
+
+for x in file:
+    cmd = x.split(" ")
+
+    if cmd[0] == "forward":
+        horizontal += int(cmd[1])
+
+    elif cmd[0] == "up" :
+        depth -= int(cmd[1])
+
+    else:
+        depth += int(cmd[1])
+
+print(int(depth) * int(horizontal))
