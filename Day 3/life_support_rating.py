c9a5b824104e463143a9cba984cbe3d85d803580
Juan <jcbritov2@gmail.com>
2021-12-03 22:05:04 -0500

    solved day 3 (2/2)

--- Day 3/life_support_rating.py
+++ Day 3/life_support_rating.py
@@ -4,9 +4,6 @@ file2 = open("inputs.txt")
 data = file.readlines()
 data2 = file2.readlines()
 
-print(data)
-print(data2)
-
 size = len(data[0]) - 1
 
 
@@ -14,62 +11,53 @@ def get_freq(data_set):
     tmp_ones = [0] * size
     tmp_zeros = [0] * size
 
-
     for x in data_set:
         for n in range(len(x)):
-
             if x[n] == '0':
                 tmp_zeros[n] = tmp_zeros[n] + 1
 
             elif x[n] == '1':
                 tmp_ones[n] = tmp_ones[n] + 1
 
-    print(tmp_ones)
-    print(tmp_zeros)
-
     return tmp_zeros, tmp_ones
 
 
 for i in range(size):
 
-    ones, zeros = get_freq(data)
-
-    if len(data) == 1:
-        break
+    zeros, ones = get_freq(data)
 
     bit = '1'
 
     if ones[i] < zeros[i]:
         bit = '0'
 
-    for j in data:
+    tmp = data[-1]
+    data = [x for x in data if x[i] == bit]
 
-        if len(data) == 1:
-            break
-
-        if j[i] != bit:
-            data.remove(j)
-
-print(data)
-"""for i in range(size):
-
-    ones, zeros = get_freq(data2)
-
-    if len(data2) == 1:
+    if len(data) == 0:
+        data = tmp
         break
+
+
+oxygen = int(''.join(data), 2)
+
+for i in range(size):
+
+    zeros, ones = get_freq(data2)
 
     bit = '0'
 
-    if ones[i] > zeros[i]:
+    if ones[i] < zeros[i]:
         bit = '1'
 
-    for j in data2:
+    tmp = data2[-1]
+    data2 = [x for x in data2 if x[i] == bit]
 
-        if len(data2) == 1:
-            break
+    if len(data2) == 0:
+        data2 = tmp
+        break
 
-        if j[i] != bit:
-            data2.remove(j)
+co2 = int(''.join(data2), 2)
 
-print(data2)"""
+print(oxygen * co2)
 
