0aa88f86e5a2787d7ce449f75a1db0febc2a5620
Juan <jcbritov2@gmail.com>
2021-12-07 21:12:28 -0500

    working on day 4 (1/2)

--- Day 4/bingo_subsystem.py
+++ Day 4/bingo_subsystem.py
@@ -3,19 +3,18 @@ import numpy as np
 
 def check_number(bingo_board_state, spots, picked_number):
     for board_num in range(len(bingo_board_state)):
-        for row in range(board_num):
-            for element in range(row):
+        for row in range(len(bingo_board_state[0])):
+            for element in range(len(bingo_board_state[0][0])):
                 if bingo_board_state[board_num][row][element] == picked_number:
                     spots[board_num][row][element] = 1
 
 
 def check_rows(spots):
-    for board_num in spots:
-        for board_row in spots[board_num]:
-            for board_element in spots[board_num][board_row]:
-                if spots[board_num][board_row][board_element] != 1:
-                    continue
-                return board_num
+    found = False
+    for board_num in range(len(spots)):
+        for row in range(len(spots[0])):
+            if spots[board_num][row][0] == 1 and spots[board_num][row][1] == 1 and spots[board_num][row][2] == 1 and spots[board_num][row][3] == 1 and spots[board_num][row][4] == 1:
+                return board_num, row, True
 
 
 def check_cols(spots):
@@ -46,12 +45,18 @@ for i in data:
 count = 0
 print(numbers)
 
+for i in range(len(boards)):
+    print()
+winner = [0, 0, False]
 for i in numbers:
-    # if count > 4:
-    # check_cols(spots=bingo_spots)
-    # check_rows(spots=bingo_spots)
+    print("Looking for {0}".format(i))
+    if count > 4:
+        # check_cols(spots=bingo_spots)
+        winner = check_rows(bingo_spots)
+    if len(winner) != 0 and winner[2]:
+        break
     check_number(boards, bingo_spots, i)
     count = count + 1
 
-print(boards)
+print(winner)
 print(bingo_spots)
